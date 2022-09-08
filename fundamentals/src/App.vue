25ff84b6c874e0d4841ad20ac7c1f8ae0872d1b4
David Barcenas <davidbarcenasmx@gmail.com>
2022-09-08 18:03:19 -0500

    user events

--- fundamentals/src/App.vue
+++ fundamentals/src/App.vue
@@ -2,15 +2,30 @@
 export default {
   data() {
     return {
+      counter: 0,
       attr: "src",
       imageUrl:
         "https://images.pexels.com/photos/8581056/pexels-photo-8581056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     };
   },
+  methods: {
+    incrementCount() {
+      this.counter++;
+    },
+    onSubmit() {
+      console.log("Sending form");
+    },
+  },
 };
 </script>
 
 <template>
-  <img v-bind:[attr]="imageUrl" alt="Random image" />
-  <button>Get random image</button>
+  <img v-bind:[attr]="imageUrl" alt="Random image" width="200" />
+  <h1>count is {{ counter }}</h1>
+  <button v-on:click="incrementCount">increment</button>
+
+  <form v-on:submit.prevent="onSubmit">
+    <input type="text" name="fullName" />
+    <button>Send</button>
+  </form>
 </template>

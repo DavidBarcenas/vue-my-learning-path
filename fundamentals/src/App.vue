f98e8e5d61e0be387b3cff5a70930a27d47ea450
David Barcenas <davidbarcenasmx@gmail.com>
2022-09-08 16:37:50 -0500

    reactive attributes

---
+++ fundamentals/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+      attr: "src",
+      imageUrl:
+        "https://images.pexels.com/photos/8581056/pexels-photo-8581056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    };
+  },
+};
+</script>
+
+<template>
+  <img v-bind:[attr]="imageUrl" alt="Random image" />
+  <button>Get random image</button>
+</template>

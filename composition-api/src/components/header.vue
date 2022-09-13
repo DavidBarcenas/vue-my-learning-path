4b70b22f839c9fc9b1dc1aaa23aba21f31b25042
David Barcenas <davidbarcenasmx@gmail.com>
2022-09-12 19:11:27 -0500

    use transitions

--- composition-api/src/components/header.vue
+++ composition-api/src/components/header.vue
@@ -1,11 +1,35 @@
 <template>
   <header>
     <h1>Composition API</h1>
+    <button @click="show = !show">Menu</button>
+    <transition name="fade">
+      <app-menu v-show="show"></app-menu>
+    </transition>
   </header>
 </template>
 
 <script>
+import AppMenu from "./menu.vue";
+
 export default {
   name: "app-header",
+  components: { AppMenu },
+  data() {
+    return {
+      show: false,
+    };
+  },
 };
 </script>
+
+<style>
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-enter-leave {
+  transition: opacity 0.5s ease;
+}
+</style>
---
+++ composition-api/src/components/menu.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "app-menu",
+};
+</script>

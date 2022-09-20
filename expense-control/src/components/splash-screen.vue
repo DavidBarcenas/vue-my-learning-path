3802be6c5bf5d6be4e1020dd6be0a64a6db9672b
David Barcenas <davidbarcenasmx@gmail.com>
2022-09-20 17:33:26 -0500

    add home and splashcreen

---
+++ expense-control/src/components/splash-screen.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="splash">
+    <img src="../assets/logo.svg" alt="Vue Logo" width="100" />
+    <h1>Expense control</h1>
+  </div>
+</template>
+
+<style>
+.splash {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
